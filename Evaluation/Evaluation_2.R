a9a1e47add0d233e0a0c36d2f2f6da352b18ab8d
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2021-05-10 17:56:25 -0700

    Create Evaluation_2.R
    Upload evaluation-First part

---
+++ Evaluation/Evaluation_2.R
@@ -0,0 +1,20 @@
+#Export the library ggplot2
+
+library(ggplot2)
+
+#Import the dataframe
+dtframe <- read.csv(file.choose())
+head(dtframe)
+
+#Rename columns name
+colnames(dtframe) <- c("Day_Week","Director","Genre","Title","Release_Date","Studio","Adjusted_Gross","Budget","Gross","IMDb_Rating","MovieLens_Rating","Overseas","Overseas%","Profit","Profit%","Runtime","US_dlls","GrossPor")
+
+#Delete useles data
+
+filter <- (dtframe$Genre=="action" | dtframe$Genre=="adventure" | dtframe$Genre=="animation" | df$Genre=="comedy" | dtframe$Genre=="drama") & (dtframe$Studio=="Buena Vista Studios" | dtframe$Studio=="Fox" | dtframe$Studio=="Paramount Pictures" | dtframe$Studio=="Sony" | dtframe$Studio=="Universal" | dtframe$Studio=="WB")
+
+filter_df <-df[filter,]
+
+#Show the new df
+
+head(filter_df)
