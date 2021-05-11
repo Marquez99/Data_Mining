7c53f37f5aad4e67f28110db90594a9b28d79448
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-05-11 07:33:44 -0700

    Deletion of Useless te, creation of the filtres for the Data

--- Evaluation/Evaluation_2.R
+++ Evaluation/Evaluation_2.R
@@ -10,11 +10,12 @@ head(dtframe)
 #Rename columns name
 colnames(dtframe) <- c("Day_Week","Director","Genre","Title","Release_Date","Studio","Adjusted_Gross","Budget","Gross","IMDb_Rating","MovieLens_Rating","Overseas","Overseas%","Profit","Profit%","Runtime","US_dlls","GrossPor")
 
-#Delete useles data
+#Delete usseless data
 
-filter <- (dtframe$Genre=="action" | dtframe$Genre=="adventure" | dtframe$Genre=="animation" | df$Genre=="comedy" | dtframe$Genre=="drama") & (dtframe$Studio=="Buena Vista Studios" | dtframe$Studio=="Fox" | dtframe$Studio=="Paramount Pictures" | dtframe$Studio=="Sony" | dtframe$Studio=="Universal" | dtframe$Studio=="WB")
+filter_df <- (dtframe$Genre=="action" | dtframe$Genre=="adventure" | dtframe$Genre=="animation" | df$Genre=="comedy" | dtframe$Genre=="drama") & (dtframe$Studio=="Buena Vista Studios" | dtframe$Studio=="Fox" | dtframe$Studio=="Paramount Pictures" | dtframe$Studio=="Sony" | dtframe$Studio=="Universal" | dtframe$Studio=="WB")
 
 filter_df <-dtframe[filter,]
 
 #Show the new df
 head(filter_df)
+
