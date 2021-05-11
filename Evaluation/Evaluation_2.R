2ef57d60368b4627ccebf91c240d4a9e98f88588
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2021-05-10 21:39:44 -0700

    Update Evaluation_2.R

--- Evaluation/Evaluation_2.R
+++ Evaluation/Evaluation_2.R
@@ -1,21 +1,36 @@
-#Export the library ggplot2
-#Library to do the Graphic representaion of the results
+#EVALUATION UNIT 2
+#MARQUEZ KEVIN
+#QUIROZ YIM 17210623
 
+#Library to do the Graphic representation of the results
 library(ggplot2)
 
 #Import the dataframe
-dtframe <- read.csv(file.choose())
-head(dtframe)
+df <- read.csv(file.choose())
+head(df)
 
-#Rename columns name
-colnames(dtframe) <- c("Day_Week","Director","Genre","Title","Release_Date","Studio","Adjusted_Gross","Budget","Gross","IMDb_Rating","MovieLens_Rating","Overseas","Overseas%","Profit","Profit%","Runtime","US_dlls","GrossPor")
+df <- read.csv("Project-Data.csv")
 
-#Delete useles data
+# Show the first rows data of the file to use
+head(df)
 
-filter <- (dtframe$Genre=="action" | dtframe$Genre=="adventure" | dtframe$Genre=="animation" | df$Genre=="comedy" | dtframe$Genre=="drama") & (dtframe$Studio=="Buena Vista Studios" | dtframe$Studio=="Fox" | dtframe$Studio=="Paramount Pictures" | dtframe$Studio=="Sony" | dtframe$Studio=="Universal" | dtframe$Studio=="WB")
+# Show the lat rows of the file
+tail(df)
 
-filter_df <-dtframe[filter,]
+#Summary of the movies
+summary(df)
 
-#Show the new df
+# Rename the columns 
+colnames(df) <- c("Day_Week","Director","Genre","Title","Release_Date","Studio","Adjusted Gross","Budget","Gross","IMDb_Rating","MovieLens_Rating","Overseas","Overseas%","Profit","Profit%","Runtime","US","GrossPor")
 
-head(filter_df)
+# Filter the data to eliminate unnecessary data
+filter_movies <- (df$Genre=="action" | df$Genre=="adventure" | df$Genre=="animation" | df$Genre=="comedy" | df$Genre=="drama") & (df$Studio=="Buena Vista Studios" | df$Studio=="Fox" | df$Studio=="Paramount Pictures" | df$Studio=="Sony" | df$Studio=="Universal" | df$Studio=="WB")
+
+# Shows the previously made filter, each data shows it as true or false
+filter_movies 
+
+# Compares the data that was loaded with the array 
+df <- df[filter_movies,]
+head(df)
+# Declaration of the input data and set values to "x" and "y"
+gg <- ggplot(df, aes(x=Genre, y=GrossPor, color=Studio,Size=Budget))
