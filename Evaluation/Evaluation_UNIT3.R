4f582f04eaec3de5471794b5837e6578d0d027f4
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-06-09 15:34:15 -0700

    Creation of the Evaluation UNIT 3

---
+++ Evaluation/Evaluation_UNIT3.R
@@ -0,0 +1,12 @@
+#PRACTICE 3 UNIT 3
+
+
+#YIM QUIROZ 17212906
+#KEVIN MARQUEZ 17212923
+
+#LOCATION ON THE ACTUAL DICTORY
+getwd()
+
+#SET THE NEW DIRECTORY
+setwd("C:/Users/kedwi/Documents/R/DataMining1/MachineLearning/LogisticRegression")
+getwd()
